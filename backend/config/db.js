6d3a0e85438e226b8353262f47961a14ab13141e
Gowtham K <gowthamk0411@gmail.com>
2025-11-06 13:32:12 +0530

    Fix

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,16 @@
 const mongoose = require("mongoose");
-const dotenv = require('dotenv');
-const connectDb =async()=>{
-    try{
-        const conn = await mongoose.connect((process.env.MONGO_URI ||"mongodb://localhost:27017/saasproject"),{
-            useNewUrlParser:true,
-            useUnifiedTopology:true
-        });
-        console.log("DB is connected")
-    }
-    catch(error){
-        console.error("Error oops",error)
-        process.exit(1)
-    }
-}
+const dotenv = require("dotenv");
 
-module.exports=connectDb
+const connectDb = async () => {
+  try {
+    await mongoose.connect(
+      process.env.MONGO_URI || "mongodb://localhost:27017/saasproject"
+    );
+    console.log("DB is connected");
+  } catch (error) {
+    console.error("Error connecting DB:", error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDb;
